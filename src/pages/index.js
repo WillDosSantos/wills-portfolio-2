50465b8a805677a1c7e4680902ef04f36314bf10
William dos Santos <williamdossantos@Williams-MacBook-Pro.local>
2022-11-20 20:16:11 -0800

    Links for Gatbsy Site=

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,11 @@ const IndexPage = ({ data }) => {
       <ul>
         {data.allMdx.nodes.map((node) => (
           <article key={node.id}>
-            <h2>{node.frontmatter.title}</h2>
+            <h2>
+              <Link to={`/blog/${node.frontmatter.slug}`}>
+                {node.frontmatter.title}
+              </Link>
+            </h2>
             <p>Posted: {node.frontmatter.date}</p>
             <p>{node.excerpt}</p>
           </article>
@@ -26,6 +30,7 @@ export const query = graphql`
         frontmatter {
           date(formatString: "MMMM D, YYYY")
           title
+          slug
         }
         id
         excerpt
